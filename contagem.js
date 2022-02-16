bbe1e77b011a2d31844b575203fb4146af9e895b
tonyacaa <99301023+tonyacaa@users.noreply.github.com>
2022-02-16 13:07:54 -0300

    Add files via upload

---
+++ contagem.js
@@ -0,0 +1,15 @@
+// Contar quantidade de alunos em uma escola.
+let numeroDeAlunos = 10
+
+for (let index = 0; index <= numeroDeAlunos; index++) {
+    // console.log(index)
+    
+if (index === 0) {
+    console.log("O número " + index + " é zero")
+} else if (index % 2 == 0) {
+    console.log("O número " + index +  " é par")
+} else {
+    console.log("O número " + index + " é ímpar")
+}
+    
+}
